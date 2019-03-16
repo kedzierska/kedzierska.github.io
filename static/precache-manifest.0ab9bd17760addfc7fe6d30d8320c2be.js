aac0f4f296be5f498016414155a145f5c01c6709
Angelika Kędzierska <kedzierska_angelika@o2.pl>
2019-03-16 14:48:03 +0100

    my first application in react

---
+++ static/precache-manifest.0ab9bd17760addfc7fe6d30d8320c2be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "23e0d1c0a4ada2cf428a",
+    "url": "/static/js/main.8e7cd722.chunk.js"
+  },
+  {
+    "revision": "1a3a0db67a1ad19266aa",
+    "url": "/static/js/2.2c8f914b.chunk.js"
+  },
+  {
+    "revision": "23e0d1c0a4ada2cf428a",
+    "url": "/static/css/main.38393a2e.chunk.css"
+  },
+  {
+    "revision": "1a3a0db67a1ad19266aa",
+    "url": "/static/css/2.c3db3c72.chunk.css"
+  },
+  {
+    "revision": "0fd55b9a9a7f933a42129d4231bb2815",
+    "url": "/index.html"
+  }
+];
